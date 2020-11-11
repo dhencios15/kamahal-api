57356a517b066c76ab0e38119531aadd497e75aa
dhencios15 <ddelacruz@umindanao.edu.ph>
2020-11-11 20:57:33 +0800

    Added Order (MVC)

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('express-async-handler');
 const Order = require('../models/orderModel');
+const Product = require('../models/productModel');
 const AppError = require('../utils/appError');
 
 exports.getUserOrder = asyncHandler(async (req, res) => {
@@ -11,13 +12,38 @@ exports.getUserOrder = asyncHandler(async (req, res) => {
   });
 });
 
-exports.createOrder = asyncHandler(async (req, res) => {
+exports.createOrder = asyncHandler(async (req, res, next) => {
+  const { orderItems } = req.body;
+
+  const prodQty = async () => {
+    return Promise.all(
+      orderItems.map(async (items) => {
+        const prod = await Product.findById({ _id: items.productItem });
+        if (!prod) return next(new AppError('No Product Found!', 404));
+        if (prod.quantity <= 0) return next(new AppError('Out of Stock!', 404));
+        const quantity = prod.quantity - items.qty;
+        const doc = await Product.findByIdAndUpdate(
+          { _id: items.productItem },
+          { quantity }
+        );
+
+        return doc.quantity;
+      })
+    );
+  };
+
+  const data = await prodQty();
+
+  if (!data) return next(new AppError('Order Cancelled!', 404));
+
   const newOrder = await Order.create({
     ...req.body,
     userId: req.user._id,
   });
 
-  res
-    .status(201)
-    .json({ status: 'success', message: 'New Order Created', data: newOrder });
+  res.status(201).json({
+    status: 'success',
+    message: 'New Order Created',
+    data: newOrder,
+  });
 });
